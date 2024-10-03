4f0efbb7eb1abbc23ff43c7b54a0a6630f375495
NdizeyeArmand <armand-gael.ndizeye@student.ehb.be>
2024-10-03 14:16:11 +0200

    Berichtcommando's inschakelen

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
 
-const client = new SapphireClient({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new SapphireClient({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+    loadMessageCommandListeners: true
+});
 
 client.login('your-token-here');
