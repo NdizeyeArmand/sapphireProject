20a2585fc3ee451fca0c225b5ffb522255c433fc
NdizeyeArmand <armand-gael.ndizeye@student.ehb.be>
2024-10-04 15:37:36 +0200

    Project opstarten met Discord token + .gitignore aanmaken + index.ts ipv index.js

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { SapphireClient } from '@sapphire/framework';
 import { GatewayIntentBits } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const client = new SapphireClient({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
     loadMessageCommandListeners: true
 });
 
-client.login('your-token-here');
+client.login(process.env.DISCORD_TOKEN);
